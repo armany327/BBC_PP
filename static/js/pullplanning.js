dde8ab3651f1bce619f2476007a7a8237409bd50
armany327 <adg00799@gmail.com>
2016-06-11 16:34:10 -0400

    initial commit

---
+++ static/js/pullplanning.js
@@ -0,0 +1,11 @@
+function pullplanning() {
+    
+    document.addEventListener("deviceready", deviceInfo, true);
+}
+
+function msg()
+{
+	alert('test')
+	navigator.notification.alert('Hey we didasdfsafd it!', function(){console.log('shit worked')}, 'This be the title', 'Click me!')
+    alert("button clicked");
+}
